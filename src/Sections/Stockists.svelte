dd30b2bf92f72a9deb9111f70656ccfe3003c431
cliveportman <clive@cliveportman.co.uk>
2021-10-13 14:29:43 +0100

    Updating Stockists page
    Adding Ocado link (with new logo)
    Adding Holland & Barrett, Booths, Protein Package and The Vegan kind

--- src/Sections/Stockists.svelte
+++ src/Sections/Stockists.svelte
@@ -7,10 +7,15 @@
 
   <h1>Find the horde</h1>
   <div class="stockists col3">
-    <img src="/img/ocado.png" alt="">
+    <a target="_blank" href="https://www.ocado.com/search?entry=Cheeky%20P%27s"><img src="/img/ocado.png" alt=""></a>
     <img src="/img/amazon.png" alt="">
     <img src="/img/nisa.png" alt="">
-    <a href="https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]=Cheeky%20P%27s&&langId=44&storeId=10151&searchType=2&searchTerm=Cheeky%20P%27s"><img src="/img/sainsburys.svg" class="sainsburys" alt=""></a>
+    <a target="_blank" href="https://www.hollandandbarrett.com/search/?isSearch=true&query=Cheeky%20P%27s"><img src="/img/holland-barrett.png" alt="Holland & Barratt" class="hollandbarrett"></a>
+    <img src="/img/booths.png" alt="Booths" class="booths">
+    <a target="_blank" href="https://proteinpackage.co.uk/search?q=Cheeky+P%27s"><img src="/img/protein-package.png" class="protein" alt="Protein Package"></a>
+    <a target="_blank" href="https://thevegankind.com/search?query=Cheeky%20P%27s"><img src="/img/the-vegan-kind.svg" class="vegankind" alt="The Vegan Kind"></a>
+    
+    <a target="_blank" href="https://www.sainsburys.co.uk/gol-ui/SearchDisplayView?filters[keyword]=Cheeky%20P%27s&&langId=44&storeId=10151&searchType=2&searchTerm=Cheeky%20P%27s"><img src="/img/sainsburys.svg" class="sainsburys" alt=""></a>
   </div>
   <a on:click|preventDefault="{ () => { showSainsburysModal = true }}" href="#" class="sainsburyslist">View list of Sainsbury's stores</a>
 
@@ -22,7 +27,7 @@
 <style>
 
 section {
-  padding: 8vw 12vw 8.8vw; margin-bottom: 0;
+  padding: 8vw 2vw 8.8vw; margin-bottom: 0;
   background: #FA364A;
 }
 
@@ -50,25 +55,56 @@ h1:before {
 .stockists {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: center; align-items: center;
 }
 
 .stockists img {
   display: block;
   height: 10vw; width: auto;
+  margin: 0 0.5rem 2rem;
   
 }
 img.sainsburys {
-  margin-top: 4vw;
-  height: 7vw;
+  margin-top: 2vw; margin-bottom: 0;
+  height: 10vw;
+}
+img.booths {
+  margin: 0 1rem 2rem 0;
+}
+img.protein {
+  height: 16vw;
+  margin: 0 1rem 2rem 0;
+}
+img.vegankind {
+  height: 12vw;
 }
 
 @media (min-width: 768px) {
+section {
+  padding: 8vw 12vw 8.8vw; margin-bottom: 0;
+  background: #FA364A;
+}
   .stockists.col3 img {
     height: 11vw;
   }
   .stockists.col3 img.sainsburys {
     height: 7vw;
+  }
+  .stockists.col3 img.hollandbarrett {
+    height: 6vw;
+    margin: 4rem 4rem 2rem 0;
+  }
+  .stockists.col3 img.booths {
+    height: 7vw;
+    margin: 4rem 4rem 2rem 0;
+  }
+  .stockists.col3 img.protein {
+    height: 9vw;
+    margin: 0 4rem 0 0;
+  }
+  .stockists.col3 img.vegankind {
+    height: 8vw;
+    margin: 0;
   }
 }
 
